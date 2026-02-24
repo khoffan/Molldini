92ea0a6bee08db578deae452b76738d67df01858
khoffan <khoffan222@gmail.com>
2026-02-24 23:33:17 +0700

    chore: change location condition

--- src/components/GuestRoute.tsx
+++ src/components/GuestRoute.tsx
@@ -1,9 +1,14 @@
 import { useSelector } from 'react-redux';
 import { Navigate, Outlet } from 'react-router';
 import type { RootState } from '../store'; // ปรับ path ตามโปรเจกต์คุณ
+import LoadingCircularSkelition from './loadingSkeleton/LoadingCircularSkelition';
 
 const GuestRoute = () => {
-    const { user } = useSelector((state: RootState) => state.auth);
+    const { user, isAuthenticated } = useSelector((state: RootState) => state.auth);
+
+    if (!isAuthenticated) {
+        return <LoadingCircularSkelition />
+    }
 
     // ถ้ามี User อยู่ใน State (ล็อกอินแล้ว) ให้ดีดไปหน้าแรกทันที
     if (user) {
--- src/components/ProtectRoute.tsx
+++ src/components/ProtectRoute.tsx
@@ -1,9 +1,14 @@
 import { useSelector } from 'react-redux';
 import type { RootState } from '../store';
 import { Navigate, Outlet } from 'react-router';
+import LoadingCircularSkelition from './loadingSkeleton/LoadingCircularSkelition';
 
 function ProtectRoute() {
-    const { user } = useSelector((state: RootState) => state.auth);
+    const { user, isAuthenticated } = useSelector((state: RootState) => state.auth);
+    if (!isAuthenticated) {
+        return <LoadingCircularSkelition />
+    }
+
     if (!user) {
         return <Navigate to="/login" replace />;
     }
